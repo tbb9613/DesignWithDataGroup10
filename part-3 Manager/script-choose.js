7378e2214220e0ba5daa62ea548e14bdfe4546b0
Dandi <s1956086@ed.ac.uk>
2020-05-06 22:10:14 +0100

    Changed the text in part 3

--- part-3 Manager/script-choose.js
+++ part-3 Manager/script-choose.js
@@ -1,16 +1,23 @@
+/*
+ * @Author: your name
+ * @Date: 2020-05-02 15:06:57
+ * @LastEditTime: 2020-05-06 22:01:57
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \flask nlp\Part-3 Manager\script-choose.js
+ */
 function chooseStore(t){
     console.log(t.id);
     document.getElementsByClassName("btn-company selected")[0].classList.remove("selected");
     t.classList.add("selected");
     let intro = document.getElementById("intro-company");
     if (t.id === "food"){
-        intro.innerHTML = "FOOD MARKET sell different kinds of food. <br><br> Donuts are good!"
+        intro.innerHTML = "We have the freshest fruits, meat, and vegetables in our market. <br><br> Want some snacks? All kinds of snacks are on the shelf, waiting for you! <br><br> Donuts are good!"//"FOOD MARKET sell different kinds of food. <br><br> Donuts are good!"
     } else if (t.id === "book") {
-        intro.innerHTML = "BOOK SHOP sell different kinds of books. <br><br> Including e-books."
+        intro.innerHTML = "Want to be someone who's smart and knowledgable? <br><br> Come and get some books! <br><br> E-books are also available."//"BOOK SHOP sell different kinds of books. <br><br> Including e-books."
     } else if (t.id === "game") {
-        intro.innerHTML = "GAME STORE sell different kinds of games. <br><br> Happy plus 1!"
+        intro.innerHTML = "Are you staying at home, almost driven crazy by boredom? <br><br> Why not play games online with your friends? <br><br> We have VR games, too!"//"GAME STORE sell different kinds of games. <br><br> Happy plus 1!"
     }
-    
 }
 
 var slider = document.getElementById("openSlider");
